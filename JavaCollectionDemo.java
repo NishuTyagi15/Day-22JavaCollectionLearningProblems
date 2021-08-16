0fe19ff0453bcc6b024ec1cd3069abd648c18b86
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-16 22:09:59 +0530

    Refactored:Demo Set using Java Collection

--- JavaCollectionDemo.java
+++ JavaCollectionDemo.java
@@ -1,34 +1,22 @@
 package com.lpjavacollection;
 
-import java.util.Iterator;
-import java.util.PriorityQueue;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class JavaCollectionDemo {
     public static void main(String[] args) {
-        doQueueDemo();
+        doSetDemo();
     }
 
-    private static void doQueueDemo() {
-        System.out.println("Demo Deque Contains");
-        PriorityQueue<String> queue = new PriorityQueue<>();
-        queue.add("Nishu Tyagi");
-        queue.add("Kajal Sharma");
-        queue.add("Shreya Singh");
-        queue.add("Priya Tiwari");
-        queue.add("Neha Mishra");
-        System.out.println("head:" + queue.element());
-        System.out.println("head:" + queue.peek());
-        System.out.println("Iterating the Queue Elements");
-        Iterator itr = queue.iterator();
-        while(itr.hasNext()) {
-            System.out.println(itr.next());
-        }
-        queue.remove();
-        queue.poll();
-        System.out.println("After Removing Two Elements");
-        Iterator itr1 = queue.iterator();
-        while(itr1.hasNext()) {
-            System.out.println(itr1.next());
+    private static void doSetDemo() {
+        System.out.println("\nIn doSetDemo");
+        Set<String> set = new LinkedHashSet<>();
+        set.add("Nishu");
+        set.add("Shreya");
+        set.add("Priya");
+        set.add("Neha");
+        for (String str :set) {
+            System.out.println(str);
         }
     }
 }
