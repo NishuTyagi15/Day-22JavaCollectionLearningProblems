42bc0a0e3d5d13c0ab1023654d10cdd985725d68
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-16 22:05:09 +0530

    Refactored:Demo Queue using Java Collection

--- JavaCollectionDemo.java
+++ JavaCollectionDemo.java
@@ -1,26 +1,34 @@
 package com.lpjavacollection;
 
 import java.util.Iterator;
-import java.util.Stack;
+import java.util.PriorityQueue;
 
 public class JavaCollectionDemo {
     public static void main(String[] args) {
-        doStackDemo();
+        doQueueDemo();
     }
 
-    private static void doStackDemo() {
-        System.out.println("Demo Stack Contains");
-        Stack<String> stack = new Stack<>();
-        stack.push("Nishu");
-        stack.push("Kajal");
-        stack.push("Shreya");
-        stack.push("Priya");
-        stack.push("Yukti");
-        stack.push("Neha");
-        String pop = stack.pop();
-        Iterator<String> itr = stack.iterator();
+    private static void doQueueDemo() {
+        System.out.println("Demo Deque Contains");
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        queue.add("Nishu Tyagi");
+        queue.add("Kajal Sharma");
+        queue.add("Shreya Singh");
+        queue.add("Priya Tiwari");
+        queue.add("Neha Mishra");
+        System.out.println("head:" + queue.element());
+        System.out.println("head:" + queue.peek());
+        System.out.println("Iterating the Queue Elements");
+        Iterator itr = queue.iterator();
         while(itr.hasNext()) {
             System.out.println(itr.next());
+        }
+        queue.remove();
+        queue.poll();
+        System.out.println("After Removing Two Elements");
+        Iterator itr1 = queue.iterator();
+        while(itr1.hasNext()) {
+            System.out.println(itr1.next());
         }
     }
 }
