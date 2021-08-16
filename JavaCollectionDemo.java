c132bd0f964f188907247eac1fdd6a42c27aa76e
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-16 21:44:03 +0530

    Added:Program to create List using Collection

--- JavaCollectionDemo.java
+++ JavaCollectionDemo.java
@@ -1,7 +1,27 @@
 package com.lpjavacollection;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class JavaCollectionDemo {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Java Collection Learning Problems!");
+        doListDemo();
+    }
+
+    private static void doListDemo() {
+        System.out.println("Demo List Contains");
+        List<String> list = new LinkedList<>();
+
+        list.add("Nishu");
+        list.add("Kajal");
+        list.add("Shreya");
+        list.add("Priya");
+        list.add("Yukti");
+
+        Iterator itr = list.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
     }
 }
